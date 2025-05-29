a77285aac0ff936547f69fb5ff1d048ca167097b
Emerson Roncancio <roncancioavila@gmail.com>
2025-05-28 21:19:19 -0500

    Elimina DTOs y entidad de verificación de salud no utilizados

--- src/healt-check/dto/update-healt-check.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHealtCheckDto } from './create-healt-check.dto';
-
-export class UpdateHealtCheckDto extends PartialType(CreateHealtCheckDto) {}
